063856dcec2cd4072b38995408af9d4b0ee46594
w-Mohamedramadan <mohamed_r_2015@yahoo.com>
2020-07-17 22:05:54 +0200

    Initial Commit

---
+++ DasaCountriesTests/DasaCountriesTests.swift
@@ -0,0 +1,34 @@
+//
+//  DasaCountriesTests.swift
+//  DasaCountriesTests
+//
+//  Created by macbook on 7/17/20.
+//  Copyright © 2020 macbook. All rights reserved.
+//
+
+import XCTest
+@testable import DasaCountries
+
+class DasaCountriesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
